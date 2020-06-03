e58144ec265982bb340ceeaa2c08864ab6b91abd
MPH <misha.medvedev2001@gmail.com>
2020-06-03 15:13:37 +0300

    Updates

---
+++ precache-manifest.a8ac8ec33808c512d34ecf06cdca694d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510959e4f39110c189c32ec76b4ae2f3",
+    "url": "/web-storage-client/index.html"
+  },
+  {
+    "revision": "376f89f58bbc3fe78261",
+    "url": "/web-storage-client/static/css/main.5d2a488a.chunk.css"
+  },
+  {
+    "revision": "fac2af5afc60da376cb7",
+    "url": "/web-storage-client/static/js/2.a87379e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-storage-client/static/js/2.a87379e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "376f89f58bbc3fe78261",
+    "url": "/web-storage-client/static/js/main.dc52d29a.chunk.js"
+  },
+  {
+    "revision": "a14f54df453415a860b4",
+    "url": "/web-storage-client/static/js/runtime-main.f97c17df.js"
+  },
+  {
+    "revision": "3b99260bf79f7f3ae6bb530ea84be84f",
+    "url": "/web-storage-client/static/media/left-arrow.3b99260b.svg"
+  },
+  {
+    "revision": "7eec2396a7f0f85f19fa47edb0e67f74",
+    "url": "/web-storage-client/static/media/right-arrow.7eec2396.svg"
+  },
+  {
+    "revision": "c384780222e4faab8c0221d764b3e62e",
+    "url": "/web-storage-client/static/media/videocam-dark.c3847802.jpg"
+  }
+]);
